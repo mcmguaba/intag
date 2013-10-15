3a216be1c16a0c6e16372bca6e25e877a299e23b
Cristina Munoz <mcmguaba.com>
2013-10-14 18:53:28 -0700

    database refactor complete.

--- json_parsy.py
+++ json_parsy.py
@@ -9,28 +9,84 @@ import json
 import os.path
 import re
 import time
+from pprint import pprint, pformat
+from MySQLdb import connect
+
+#VERSION = 1
 
 def main():
     #read in data
+    db = connect(host="localhost", db="intagdb",
+            read_default_file="~/.my.cnf")
+    db.set_character_set('utf8')
+    #eliminate existing data, this script does everything.
+    truncate_data(db)
     json_unclean_file_obj = open(sys.argv[1], 'rU')
     json_clean_string = clean_json(json_unclean_file_obj)
     json_data = json.loads(json_clean_string)
     json_unclean_file_obj.close()
-    #parse json
+    #keep track of aliases, since it's the pkey
+    aliases = []
     for data in json_data:
         try:
             t = time.strptime(data["modified"], "%Y-%m-%d %H:%M:%S")
         except ValueError:
             t = time.strptime(data["created"], "%Y-%m-%d %H:%M:%S")
-        file_path = os.path.join('content', str(t.tm_year), str(t.tm_mon), data["alias"]+'.html')
-        dir = os.path.dirname(file_path)
-        if not os.path.exists(dir):
-            os.makedirs(dir)
-        structured_html = add_head_and_body(data)
-        structured_html = remove_styling_and_empty_elements(structured_html)
-        html_file = open(file_path , 'w')
-        html_file.write(structured_html.encode("utf-8"))
-        html_file.close()
+        data['date'] = time.strftime("%Y-%m-%d", t)
+        parse_other_fields(data)
+        remove_styling_and_empty_elements(data['body'])
+        utf_encode(data)
+        if data['alias'] not in aliases:
+            insert_into_database(db, data)
+            aliases.append(data['alias'])
+        else:
+            #don't insert duplicates, just let us know which ones they are
+            print data['alias']
+    db.commit()
+
+
+def truncate_data(db):
+    '''
+    Remove everything from table.
+    '''
+    c = db.cursor()
+    c.execute("DELETE FROM articles;");
+    c.close()
+
+def insert_into_database(db, data):
+    '''
+    Store data in the database.
+    '''
+    c = db.cursor()
+
+    # weird encoding errors. this seems to fix them.
+    c.execute('SET NAMES utf8;')
+    c.execute('SET CHARACTER SET utf8;')
+    c.execute('SET character_set_connection=utf8;')
+
+    c.execute("""INSERT INTO articles VALUES (%s, %s, %s, %s, %s, %s)""", \
+            (data['alias'], data['author'], data['title'], \
+            data['intro_text'], data['body'], data['date']))
+
+    c.close()
+
+
+def utf_encode(data):
+    '''
+    Necessary.
+    '''
+    for k, v in data.items():
+        if type(v) == str:
+            data[k] = v.encode('utf-8')
+
+    '''
+    if data['alias'] == 'el-petroleo-del-yasuni-se-queda-bajo-tierra':
+        global VERSION
+        filename = "yasuni_{}".format(VERSION)
+        f = open(filename,'w')
+        f.write(pformat(data))
+        VERSION += 1
+    '''
 
 def clean_json(doc_text):
     sarray = []
@@ -40,28 +96,31 @@ def clean_json(doc_text):
     new_clean_string = clean_string_json.replace("\'", "\\'")
     return new_clean_string
 
-def add_head_and_body(html):
+def parse_other_fields(data):
     '''
-    Add necessary html tags to generate a page.
+    Add author, intro and body fields.
     '''
-    prepend = "<html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" /><title>" + html["title"] + "</title><link href=\"../../../css/style.css\"></head><body>"
-    h1 = "<h1>" + html["title"] + "</h1>"
-    author, intro_text = get_author(html["introtext"])
-    author_html = '<p class="author">' + author + '</p>'
-    main_content = html["fulltext"] or intro_text
-    body = author_html + main_content
-    return prepend + h1 + body  + "</body></html>"
+    if data['introtext'] and data['fulltext']:
+        data['author'], data['intro_text'] = get_author_and_content(data["introtext"])
+        data['body'] = data['fulltext']
+    else:
+        main_content = data['introtext'] or data['fulltext']
+        data['author'], data['body'] = get_author_and_content(main_content)
+        data['intro_text'] = ''
+        #TODO: find a way to parse out an intro
+        #TODO: replace <br /> with p tags
 
-def get_author(html):
+def get_author_and_content(intro, body=None):
     '''
     Return the raw author name.
     '''
-    if html:
-        regex = '\<h6\>(.*?)\</h6\>(.*)'
-        match = re.search(regex, html)
-        if match:
-            return remove_html_tags(match.group(1)), match.group(2)
-    return "Los Editores", html
+    regex = '\<h6\>(.*?)\</h6\>(.*)'
+    match = re.search(regex, intro)
+    if match:
+        author = remove_html_tags(match.group(1))
+        body = match.group(2)
+        return author, body
+    return "Los Editores", intro
 
 def remove_html_tags(data):
     '''
